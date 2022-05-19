c96fd1ec2ff3e5903452772e55ee83f1389ef543
kavinteng <76694895+kavinteng@users.noreply.github.com>
2022-05-19 10:18:06 +0700

    Add files via upload

---
+++ code/test_realtime.py
@@ -0,0 +1,33 @@
+import torch
+import cv2
+
+model = torch.hub.load('ultralytics/yolov5', 'yolov5l', pretrained=True)
+model.conf = 0.5
+model.iou = 0.4
+
+size_img_vdo = (640, 360)
+color = (0, 0, 255)
+cap = cv2.VideoCapture(0)
+
+while True:
+    _,frame = cap.read()
+    frame = cv2.resize(frame, size_img_vdo)
+    results = model(frame, size=640)
+    out2 = results.pandas().xyxy[0]
+    if len(out2) != 0:
+        for i in range(len(out2)):
+            output_landmark = []
+            xmin = int(out2.iat[i, 0])
+            ymin = int(out2.iat[i, 1])
+            xmax = int(out2.iat[i, 2])
+            ymax = int(out2.iat[i, 3])
+
+            if obj_name == 'person' or obj_name == '0':
+                cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color, 2)
+
+    cv2.imshow('frame',frame)
+    k = cv2.waitKey(1)
+    if k == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
