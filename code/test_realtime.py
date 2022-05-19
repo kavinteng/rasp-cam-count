3d2944d89562b87e8e1c87155759d8716adfdd2d
kavinteng <76694895+kavinteng@users.noreply.github.com>
2022-05-19 11:15:30 +0700

    Add files via upload

--- code/test_realtime.py
+++ code/test_realtime.py
@@ -12,18 +12,18 @@ cap = cv2.VideoCapture(0)
 while True:
     _,frame = cap.read()
     frame = cv2.resize(frame, size_img_vdo)
-    # results = model(frame, size=640)
-    # out2 = results.pandas().xyxy[0]
-    # if len(out2) != 0:
-    #     for i in range(len(out2)):
-    #         output_landmark = []
-    #         xmin = int(out2.iat[i, 0])
-    #         ymin = int(out2.iat[i, 1])
-    #         xmax = int(out2.iat[i, 2])
-    #         ymax = int(out2.iat[i, 3])
-    #         obj_name = out2.iat[i, 6]
-    #         if obj_name == 'person' or obj_name == '0':
-    #             cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color, 2)
+    results = model(frame, size=640)
+    out2 = results.pandas().xyxy[0]
+    if len(out2) != 0:
+        for i in range(len(out2)):
+            output_landmark = []
+            xmin = int(out2.iat[i, 0])
+            ymin = int(out2.iat[i, 1])
+            xmax = int(out2.iat[i, 2])
+            ymax = int(out2.iat[i, 3])
+            obj_name = out2.iat[i, 6]
+            if obj_name == 'person' or obj_name == '0':
+                cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color, 2)
 
     cv2.imshow('frame',frame)
     k = cv2.waitKey(1)
